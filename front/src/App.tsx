3d712e62d738f1537632e5303ca75f34a4c810b5
coldhed <santiago.rdgz2001@gmail.com>
2023-10-01 18:22:45 -0600

    Changed the project's colors

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -11,6 +11,7 @@ import {
 import { Route } from 'react-router-dom';
 import { dataProvider } from "./dataProvider";
 import authProvider from "./authProvider";
+import myTheme from "./myTheme";
 import { TicketList } from "./elements/TicketList";
 
 import { CeLayout } from "./layouts/CeLayout";
@@ -19,7 +20,7 @@ import { CaLayout } from "./layouts/CaLayout";
 
 export const App = () => (
 
-    <Admin dataProvider={dataProvider} authProvider={authProvider} >
+    <Admin dataProvider={dataProvider} authProvider={authProvider} theme={myTheme} >
         {permissions => {
             if (permissions === "no role") {
                 console.log("No permissions")
