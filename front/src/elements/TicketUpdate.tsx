bd279872de441feb09e5e7ed6f69f5241c05d531
Mariel Gomez <marielgomezc1z@gmail.com>
2023-10-04 11:54:03 -0600

    Display tickets information in ticket section

--- front/src/elements/TicketUpdate.tsx
+++ front/src/elements/TicketUpdate.tsx
@@ -1,11 +1,57 @@
-import {SimpleForm, Toolbar, SelectInput, SaveButton, Edit, TextField, TabbedForm, FormTab, required, TextInput, DateField } from 'react-admin';
+import {Create, SimpleForm, Toolbar, SelectInput, SaveButton, Edit, TextField, TabbedForm, FormTab, required, TextInput, DateField } from 'react-admin';
 import {useRecordContext} from "react-admin";
 import {useOnSuccess,} from '../hooks/costumhandlers';
+import "../CSS/TicketUpdate.css";
 
 const TicketTitle = () => {
     const record = useRecordContext();
     return <span>{record ? `"${record.titulo}"` : ''}</span>;
 };
+const InicioTextComponent = () => (
+    <div className="customTextComp">
+      <p>Inicio: </p>
+    </div>
+);
+const TituloTextComponent = () => (
+    <div className="customTextComp">
+      <p>Título: </p>
+    </div>
+);
+const AulaTextComponent = () => (
+    <div className="customTextComp">
+      <p>Aula: </p>
+    </div>
+);
+const CategoriaTextComponent = () => (
+    <div className="customTextComp">
+      <p>Categoría: </p>
+    </div>
+);
+const PrioridadTextComponent = () => (
+    <div className="customTextComp">
+      <p>Prioridad: </p>
+    </div>
+);
+const EstatusTextComponent = () => (
+    <div className="customTextComp">
+      <p> Estatus: </p>
+    </div>
+);
+const EstatusCambioTextComponent = () => (
+    <div className="customTextComp">
+      <p> Cambiar Estatus: </p>
+    </div>
+);
+
+const CostumCreateComment = () => (
+    <Create>
+        <SimpleForm>
+            <TextInput source="comentarios" multiline rows={5} />
+        </SimpleForm>
+    </Create>
+);
+
+
 
 
 export const TicketEdit = () => {
@@ -15,23 +61,31 @@ export const TicketEdit = () => {
         <Edit title={<TicketTitle/>} mutationOptions={{onSuccess}}>
            <TabbedForm>
             <FormTab label = "Ticket">
-                <TextField source="inicio" showTime />
-                <TextField source="titulo" label="Título"/>
-                <TextField source="aula" label="Aula"/>
-                <TextField source="categoria" label="Categoría"/>
-                <TextField source="prioridad" label="Prioridad"/>
-                <TextField source="comentarios" label="Comentarios:"/>
-                <SelectInput source="estatus" choices={[
-                    { id: 'ab', name: 'Abierto' },
-                    { id: 'ep', name: 'En proceso' },
-                    { id: 'fi', name: 'Finalizado' },
-                ]} validate={[required('Campo Obligatorio')]} />
+                <InicioTextComponent />
+                <DateField label="Inicio" source="inicio" showTime />
+                < TituloTextComponent />
+                <TextField label="Título" source="titulo" />
+                < AulaTextComponent />
+                <TextField label="Aula" source="aula" />
+                < CategoriaTextComponent />
+                <TextField label="Categoría" source="categoria" />
+                < PrioridadTextComponent />
+                <TextField label="Prioridad" source="prioridad" />
+                < EstatusTextComponent />
+                <TextField label="Estatus actual" source="estatus" />
+                <EstatusCambioTextComponent />
+                <SelectInput label="Estatus" source="estatus" choices={[
+                    { id: 'Abierto', name: 'Abierto' },
+                    { id: 'En proceso', name: 'En proceso' },
+                    { id: 'Cerrado', name: 'Cerrado' },
+                ]} />
             </FormTab>
-            <FormTab label="Añadir comentario">
-                <TextInput source="comentarios" validate={[required('Campo Obligatorio')]} />
+
+            <FormTab label="Comentarios">
+                <CostumCreateComment/>
             </FormTab>
             <FormTab label="Finalizar ticket">
-                <TextInput source="resolucion" validate={[required('Campo Obligatorio')]} />
+                <TextInput source="resolucion" />
             </FormTab>
        </TabbedForm>
       </Edit>
