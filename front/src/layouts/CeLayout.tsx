02baf963b0c5ba6d8e4aba5b7c46646629cf7651
Paula Verdugo <paulaverdugo@Paulas-MacBook-Air.local>
2023-10-11 17:52:17 -0600

    List Aulas

--- front/src/layouts/CeLayout.tsx
+++ front/src/layouts/CeLayout.tsx
@@ -13,6 +13,9 @@ import { UserShow } from "../elements/User/UserShow";
 import { TicketList } from "../elements/TicketsList";
 import { TicketCreate } from "../elements/TicketCreate";
 import { TicketEdit } from "../elements/TicketUpdate";
+import { AulaCreate } from "../elements/Aula/AulaCreate";
+import { AulaList } from "../elements/Aula/AulaList";
+import { AulaShow } from "../elements/Aula/AulaShow";
 
 export const CeLayout = () => {
     return (
@@ -24,6 +27,10 @@ export const CeLayout = () => {
                 edit={TicketEdit}
             />
             <Resource name="Usuarios" list={UserList} create={UserCreate} show={UserShow} />
+
+            <Resource name="Aula" list={AulaList}/>
+
+            {/* <Resource name="Aulas" list={AulaList} create={AulaCreate} show={AulaShow} /> */}
         </>
     );
 };
