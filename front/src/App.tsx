936f8d865d8886e9544ee42013766c02d1be06cb
coldhed <santiago.rdgz2001@gmail.com>
2023-10-12 15:44:29 -0600

    Added logo to the app bar

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,5 +1,6 @@
 import {
     Admin,
+    Layout,
     Resource,
     ListGuesser,
     EditGuesser,
@@ -14,6 +15,7 @@ import { dataProvider } from "./dataProvider";
 import authProvider from "./authProvider"
 import { i18nProvider } from "./i18nProvider/i18nProvider";
 import MyLoginPage from "./MyLoginPage";
+import { MyAppBar } from "./MyAppBar";
 
 import myTheme from "./myTheme";
 
@@ -21,7 +23,7 @@ import myTheme from "./myTheme";
 import { CeLayout } from "./layouts/CeLayout";
 import { CaLayout } from "./layouts/CaLayout";
 
-
+const MyLayout = (props: any) => <Layout {...props} appBar={MyAppBar} />;
 
 export const App = () => (
 
@@ -31,6 +33,7 @@ export const App = () => (
         theme={myTheme}
         i18nProvider={i18nProvider}
         loginPage={<MyLoginPage theme={myTheme} />}
+        layout={MyLayout}
     >
         {permissions => {
             // if there are no permissions, you return the CaLayout 
---
+++ front/src/MyAppBar.jsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { AppBar, TitlePortal } from 'react-admin';
+import Box from '@mui/material/Box';
+
+export const MyAppBar = () => (
+    <AppBar color="primary">
+        <TitlePortal />
+        <Box flex="1" />
+        <img
+            className="h-14 p-2 ml-[-130px]"
+            src="src/resources/logo-white.png"
+            alt="logo"
+        />
+        <Box flex="1" />
+    </AppBar>
+);
