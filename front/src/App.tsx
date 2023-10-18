58013ed0b00a11da2624d397c4edabca12fac789
coldhed <santiago.rdgz2001@gmail.com>
2023-10-18 17:19:38 -0600

    Removed dark theme button, and translation button

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -33,7 +33,6 @@ export const App = () => (
         dataProvider={dataProvider}
         authProvider={authProvider}
         theme={myTheme}
-        darkTheme={{ palette: { mode: 'dark' } }}
         i18nProvider={i18nProvider}
         loginPage={<MyLoginPage theme={myTheme} />}
         layout={MyLayout}
