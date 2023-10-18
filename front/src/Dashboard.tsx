f2e8ed81cd606599c9c386635299ef8022449d58
coldhed <santiago.rdgz2001@gmail.com>
2023-10-18 01:43:17 -0600

    Added auth to request to get average ticket resolution time

--- front/src/Dashboard.tsx
+++ front/src/Dashboard.tsx
@@ -5,70 +5,73 @@ import React, { useEffect, useState } from 'react';
 
 
 
-  
+
 
 function Dashboard() {
 
     const [tiempoPromedio, setTiempoPromedio] = useState(null);
 
-  useEffect(() => {
-    fetch('https://127.0.0.1:4000/dashboard/tiempo-promedio-cierre')
-      .then((response) => response.json())
-      .then((data) => setTiempoPromedio(data.tiempoFinal))
-      .catch((error) => console.error(error));
-  }, []);
+    useEffect(() => {
+        fetch('https://127.0.0.1:4000/dashboard/tiempo-promedio-cierre', {
+            method: "GET",
+            headers: new Headers({ Authentication: localStorage.getItem("auth") as string }),
+        })
+            .then((response) => response.json())
+            .then((data) => setTiempoPromedio(data.tiempoFinal))
+            .catch((error) => console.error(error));
+    }, []);
 
     return (
         <div>
-            <br/>
-            <br/>   
+            <br />
+            <br />
             <h1 className="text-center font-monospace fw-bolder lh-base" style={{ fontSize: '3rem' }}>
                 ¡Bienvenido Coordinador Ejecutivo!
             </h1>
-            <br/>
-            <br/>
-            <hr className="mt-3 mb-2"/>
-            <br/>
-            <br/>
+            <br />
+            <br />
+            <hr className="mt-3 mb-2" />
+            <br />
+            <br />
 
             <div>
                 <p className="text-center fw-bold " style={{ fontSize: '2rem' }} >Tiempo promedio de cierre de tickets: </p>
-                <br/>
+                <br />
                 <div>
-                {tiempoPromedio !== null ? (
-                    <p className="text-center fw-bolder " style={{ fontSize: '2rem', color: 'green' }}> {tiempoPromedio} horas</p>
-                ) : (
-                    <p className="text-center font-monospace fw-bold lh-base" style={{ fontSize: '1rem' }}> Cargando tiempo promedio... </p>
-                )}
+                    {tiempoPromedio !== null ? (
+                        <p className="text-center fw-bolder " style={{ fontSize: '2rem', color: 'green' }}> {tiempoPromedio} horas</p>
+                    ) : (
+                        <p className="text-center font-monospace fw-bold lh-base" style={{ fontSize: '1rem' }}> Cargando tiempo promedio... </p>
+                    )}
                 </div>
             </div>
-            
-            <br/>
-            <br/>
-            <hr className="mt-3 mb-2"/>
-          
+
+            <br />
+            <br />
+            <hr className="mt-3 mb-2" />
+
 
             <div>
                 <LineChart />
             </div>
-            <br/>
-            <hr className="mt-3 mb-2"/>
-            <br/>
+            <br />
+            <hr className="mt-3 mb-2" />
+            <br />
 
             <div>
                 <AulaMasIncidentes />
             </div>
 
-            
-            <br/>
-            <hr className="mt-3 mb-2"/>
-            <br/>
+
+            <br />
+            <hr className="mt-3 mb-2" />
+            <br />
             <div>
-                <WeeklyStatus />                       
+                <WeeklyStatus />
             </div>
-        
-        </div> 
-     );
+
+        </div>
+    );
 }
 
 export default Dashboard;
